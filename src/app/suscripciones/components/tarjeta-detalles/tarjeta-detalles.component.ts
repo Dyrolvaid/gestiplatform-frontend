89b08f044ed79709ca49b6099ce458e3a0650fd1
borilio <exprimetuandroid@gmail.com>
2022-06-29 22:02:26 +0200

    Recibido y mostrado en el componente tarjeta-detalles.component.ts la información desde el tarjeta.component.ts.

--- src/app/suscripciones/components/tarjeta-detalles/tarjeta-detalles.component.ts
+++ src/app/suscripciones/components/tarjeta-detalles/tarjeta-detalles.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {DynamicDialogConfig} from "primeng/dynamicdialog";
+import {Suscripcion} from "../../../shared/interfaces/suscripciones-por-persona.interface";
 
 @Component({
   selector: 'app-tarjeta-detalles',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TarjetaDetallesComponent implements OnInit {
 
-  constructor() { }
+  public suscripcion?: Suscripcion;
+
+  constructor(private _dynamicDialogConfig: DynamicDialogConfig) {}
 
   ngOnInit(): void {
+    this.suscripcion = this._dynamicDialogConfig.data.suscripcion;
   }
 
 }
