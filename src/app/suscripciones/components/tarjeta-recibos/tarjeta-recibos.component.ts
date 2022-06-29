582525c21bb1b14e7088932ad30ffde948af9aee
Dyrolvaid <nomellamodavid.c@gmail.com>
2022-06-30 00:51:42 +0200

    Establecidos los valores adecuados para las variables suscripcion y recibos: Mediante el servicio suscripciones y el campo data inyectados, en suscripciones. Mediante la suscripción al observable en una consulta AJAX para el array de recibos.

--- src/app/suscripciones/components/tarjeta-recibos/tarjeta-recibos.component.ts
+++ src/app/suscripciones/components/tarjeta-recibos/tarjeta-recibos.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {DynamicDialogConfig} from "primeng/dynamicdialog";
+import {Recibo} from "../../../shared/interfaces/recibo.interface";
+import {RecibosService} from "../../../shared/services/recibos.service";
+import {HttpErrorResponse} from "@angular/common/http";
+import {SuscripcionesService} from "../../../shared/services/suscripciones.service";
+import {Suscripcion} from "../../../shared/interfaces/suscripciones-por-persona.interface";
 
 @Component({
   selector: 'app-tarjeta-recibos',
@@ -7,9 +13,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TarjetaRecibosComponent implements OnInit {
 
-  constructor() { }
+  public suscripcion?: Suscripcion;
+  public estadoCargandoRecibos: boolean;
+  public recibos?: Recibo[];
+
+  constructor(private _dynamicDialogConfig: DynamicDialogConfig,
+              private _recibosService: RecibosService,
+              private _suscripcionesService: SuscripcionesService) {
+    this.estadoCargandoRecibos = false;
+  }
 
   ngOnInit(): void {
+    this.suscripcion = this._suscripcionesService.suscripciones![this._dynamicDialogConfig.data.suscripcion.id - 1].suscripcion;
+    this.estadoCargandoRecibos = true;
+    this._recibosService.getRecibosPorSuscripcion(this.suscripcion!.id).subscribe({
+      next: (resp) => {
+        this.estadoCargandoRecibos = false;
+        //console.log(JSON.stringify(resp));
+        this.recibos = resp;
+      },
+      error: (error: HttpErrorResponse) => {
+        this.estadoCargandoRecibos = false;
+        console.error(error);
+      }
+    });
   }
 
 }
