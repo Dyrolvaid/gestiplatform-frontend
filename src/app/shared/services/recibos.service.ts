dcf5fb593e069830363213cddce2e1e136370ba8
Dyrolvaid <nomellamodavid.c@gmail.com>
2022-06-30 19:56:35 +0200

    Implementado medianamente el componente recibos a falta de asignar el recibo correcto a cada persona.

--- src/app/shared/services/recibos.service.ts
+++ src/app/shared/services/recibos.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Recibo} from "../interfaces/recibo.interface";
 import {Observable} from "rxjs";
+import {Grupo} from "../interfaces/grupo.interface";
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,13 @@ export class RecibosService {
 
   constructor(private _http: HttpClient) {}
 
-  public getRecibosPorSuscripcion(idSuscripcion: number): Observable<Recibo[]> {
-    const url = `/api/v1/recibos/grupos/${idSuscripcion}`;
+  public getRecibosByGrupo(idGrupo: number): Observable<Recibo[]> {
+    const url = `/api/v1/recibos/grupos/${idGrupo}`;
     return this._http.get<Recibo[]>(url);
   }
 
+  public getGrupoByPersona(): Observable<Grupo[]> {
+    const url = `/api/v1/grupos`;
+    return this._http.get<Grupo[]>(url);
+  }
 }
